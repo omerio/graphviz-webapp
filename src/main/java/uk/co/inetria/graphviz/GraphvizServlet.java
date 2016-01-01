5b8c3e0ee16d049bbcd5312e853a4b43ed2c6daf
omerio <omerio@me.com>
2016-01-01 12:37:04 +0000

    initial commit

---
+++ src/main/java/uk/co/inetria/graphviz/GraphvizServlet.java
@@ -0,0 +1,22 @@
+package uk.co.inetria.graphviz;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GraphvizServlet
+ */
+public class GraphvizServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	
+	$('.js-render').click(function (e) {
+	  
+	  var dot = $('.js-dot-source').val();
+	  $('.js-render').prop('disabled', true);
+	  
+	  $.ajax({
+		  url: "/getgraph",
+		  data: { 'dot' : dot },
+		  type: "POST",
+		  success: function(data) {
+			  var graph = $(".js-svg-graph");
+			  graph.empty();
+			  var importedSVGRootElement = document.importNode(data.documentElement,true);
+			  //append the imported SVG root element to the appropriate HTML element
+			  graph.append(importedSVGRootElement);
+
+		  },
+		  error: function() {
+			  $('#myModal').modal('show');
+		  },
+		  complete: function() {
+			  $('.js-render').prop('disabled', false);
+		  }
+	  });
+	  
+
+	  e.preventDefault();
+	  
+	});
+
+});
